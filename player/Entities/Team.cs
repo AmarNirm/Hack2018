8b49f99634db83e3df8cde552d9af07428a73f85
Ori Glick <arch23@gmail.com>
2018-06-12 13:59:50 +0300

    public function in team to find closest player to the ball Impoertant fixes in Memory and VisualInfo

--- player/Entities/Team.cs
+++ player/Entities/Team.cs
@@ -1,8 +1,10 @@
 ﻿using RoboCup.Entities;
+using RoboCup.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+//using System.Threading;
 
 namespace RoboCup
 {
@@ -13,6 +15,7 @@ namespace RoboCup
         public  String m_teamName;
         public IFormation m_teamFormation;
         public Coach m_coach;
+        public int ClosestPlayerToTheBall;
 
         public Team(string[] args)
         {
@@ -31,9 +34,56 @@ namespace RoboCup
             }
 
             m_coach = new Coach();
-            //m_teamFormation = new Formation_4_4_2();
-            m_teamFormation = new NirFormation();
+            m_teamFormation = new Formation_4_4_2();
             m_playerList = m_teamFormation.InitTeam(this, m_coach);
         }
+
+
+        public virtual int FindPlayerClosestToTheBall()
+        {
+            SeenCoachObject ballPosByCoach = null;
+            SeenCoachObject objPlayer = null;
+            double Distance = -1;
+            double CurrentDistance = -1;
+            string player = "player England ";
+
+            Object thisLock = new Object();
+
+            lock(thisLock)
+            {
+                //go over all players in the team playerList
+                for (int i=1;i<5;i++)
+                {
+                    ballPosByCoach = m_coach.GetSeenCoachObject("ball");
+                    Double BallX = ballPosByCoach.Pos.Value.X;
+                    Double BallY = ballPosByCoach.Pos.Value.Y;
+                    
+                    objPlayer = m_coach.GetSeenCoachObject(player + i.ToString());
+                    Double PlayerX = objPlayer.Pos.Value.X;
+                    Double PlayerY = objPlayer.Pos.Value.Y;
+                    
+                    //calculate the distance
+                    CurrentDistance = Math.Sqrt(Math.Pow(PlayerX - BallX, 2) + Math.Pow(PlayerY - BallY, 2));
+
+                    //set the current distance with the shortest one
+                    if (i == 1)
+                    {
+                        Distance = CurrentDistance;
+                        ClosestPlayerToTheBall = i;
+                    }
+                    else if (CurrentDistance < Distance)
+                    {
+                        Distance = CurrentDistance;
+                        ClosestPlayerToTheBall = i;
+                    }
+               
+                }
+            }
+
+            return ClosestPlayerToTheBall;
+        }
+
+
+
     }
 }
